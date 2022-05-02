2ef9d78db653d49780123ec438e2bc618f6f0cbd
biaggio <biaggiogzdev@gmail.com>
2022-05-02 12:37:50 +0200

    Commit1#Create_class_Sellermanagement_Car

---
+++ src/com/company/Car.java
@@ -0,0 +1,38 @@
+package com.company;
+
+public class Car {
+    String brand;
+    int numberofdoors;
+    String color;
+    double km;
+    String carLicense;
+    String insurance;
+    double price;
+
+    //Constructor empty
+    public Car(){}
+
+    public Car(String brand, int numberofdoors, String color, double km, String carLicense, String insurance, double price){
+        this.brand = brand;
+        this.numberofdoors = numberofdoors;
+        this.color = color;
+        this.km = km;
+        this.carLicense = carLicense;
+        this.insurance = insurance;
+        this.price = price;
+
+    }
+    //method to modified km, carLicense, insurance and price
+    public void setKLIP(double km, String carLicense, String insurance, double price){
+
+    }
+
+    public boolean checkExistsCarLicense(String carLicense){
+        this.carLicense = carLicense;
+        return  false;
+    }
+
+
+
+
+}
