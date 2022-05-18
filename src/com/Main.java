1ea5d716ccbd567de4d20ffbc319e7e5b52e511f
biaggio <biaggiogzdev@gmail.com>
2022-05-18 11:57:26 +0200

    Commit17#

--- src/com/Main.java
+++ src/com/Main.java
@@ -11,12 +11,12 @@ import com.view.ViewMenu;
 
 public class Main {//Main
     public static void main(String[] args) {
-        fakeusers();
+        fakeCustomers();
         Scanner reader = new Scanner(System.in);
         ViewMenu.Mainmenu(reader);
 
     }
-    public static void fakeusers(){
+    public static void fakeCustomers(){
         ArrayList<Card> cards1 = new ArrayList<>(),  cards2 = new ArrayList<>(), cards3 = new ArrayList<>();
         cards1.add(new Card(5555748587548754L,"052022","visa","693"));
         cards1.add(new Card(5588888587548754L,"092022","mastercard","451"));
@@ -29,12 +29,7 @@ public class Main {//Main
         Customer c4 = new Customer("12345678L","MIGUEL","MENDOZA","26","620963258","YELLOW","FORD");
         Customer c5 = new Customer("12345678V","JOSE","DIAZ","24","695145258","BLUE","BMW");
 
-        Customer[] objects = new Customer[5];
-        objects[0] = c1;
-        objects[1] = c2;
-        objects[2] = c3;
-        objects[3] = c4;
-        objects[4] = c5;
+        Customer[] objects = new Customer[]{c1,c2,c3,c4,c5};
         for(int i = 0; i < 5; i++){
             DatabaseController.addCustomerWithouCard(objects[i]);
         }
