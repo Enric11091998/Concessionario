8341ab1aa39218b503856fc661892d2d104faabf
biaggio gutierrez <biaggiogzdev@gmail.com>
2022-05-24 22:00:46 +0200

    Commit#28 Working insert Customer with Card

--- src/main/java/persistenceTest/Evaluacion.java
+++ src/main/java/persistenceTest/Evaluacion.java
@@ -1,5 +1,4 @@
 package persistenceTest;
-import com.model.Card;
 import org.testng.annotations.Test;
 import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
@@ -15,10 +14,11 @@ public class Evaluacion {
         try {
             EntityManager em = emf.createEntityManager();
             em.getTransaction().begin();
+            //Customer customer = em.find(Customer.class,"12345678Z");
             Card car = new Card(5562000005625356L,"052024","MASTERCARD","092");
             List<Card> cardsUser = new ArrayList<>();
             cardsUser.add(car);
-            Customer customer = new Customer("12345678Z","ENRIC","FIGUERAS","22","789456123","GREEN","LEXUS",cardsUser);
+            Customer customer = new Customer("12345678R","BIAGGIO","MOISES","22","789456123","GREEN","LEXUS",cardsUser);
             em.persist(customer);
             em.getTransaction().commit();
             em.close();
